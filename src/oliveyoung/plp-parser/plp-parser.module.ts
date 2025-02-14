ee2dfe7de99b6091efc0de50705b93f0138ddb41
The Anh <anh.vi@sotatek.com>
2025-02-14 11:00:55 +0700

    feat: update config for plp parser

--- src/oliveyoung/plp-parser/plp-parser.module.ts
+++ src/oliveyoung/plp-parser/plp-parser.module.ts
@@ -1,20 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { KafkaConsumerService } from 'src/kafka/kafka.consumer';
 import { KafkaModule } from 'src/kafka/kafka.module';
 import { PLPParserHandler } from './plp-parser.handler';
 import { ConfigModule } from '@nestjs/config';
 import { PLPParserService } from './plp-parser.service';
+import { ConfigSynchronizerHandler } from '../../config-synchronizer/config-synchronizer.handler';
+import { ConfigSynchronizerModule } from '../../config-synchronizer/config-synchronizer.module';
 
 @Module({
   controllers: [],
   providers: [PLPParserHandler, PLPParserService],
-  imports: [ConfigModule, KafkaModule],
+  imports: [ConfigModule, KafkaModule, ConfigSynchronizerModule],
 })
-export class PLPParserModule {
+export class PLPParserModule implements OnModuleInit {
   constructor(
     private kafkaConsumerService: KafkaConsumerService,
-    plpParserHandler: PLPParserHandler,
-  ) {
-    void this.kafkaConsumerService.listen(plpParserHandler);
+    private readonly configSynchronizerHandler: ConfigSynchronizerHandler,
+    private readonly plpParserHandler: PLPParserHandler,
+  ) {}
+  async onModuleInit() {
+    // Use the PDPParserHandler only after configurations is ready
+    await this.configSynchronizerHandler.waitForConfig();
+    void this.kafkaConsumerService.listen(this.plpParserHandler);
   }
 }
